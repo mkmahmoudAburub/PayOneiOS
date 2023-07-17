2e0d04521e5e104053dc4152d9d5606ac6febf01
Nikita Korolev <nk25.dev@gmail.com>
2023-07-17 12:27:30 +0300

    Initial Commit

---
+++ Payone/PayoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayoneApp.swift
+//  Payone
+//
+//  Created by Nikita Korolev on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct PayoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+**Description**
+-----
+An iOS version of the PayOne cryptocurrency/cryptowallet management application (property of Arekan)
+
